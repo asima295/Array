ecbb37a5c53157c85eb3bcc5bf4bb8fd73ba7326
asima295 <ftasleem581@gmail.com>
2024-10-23 09:36:30 +0500

    Add files via upload

---
+++ array/array.js
@@ -0,0 +1,59 @@
+
+const book = ["english", "urdu", "math", "isi", "ps", "ch", "com"];
+book.push["phy"];
+console.log(book);
+
+
+const furits = ["apple", "orange", "mango", "banana", "grape", "guava", "pinapple","melon"];
+furits.push["pear"];
+console.log(furits);
+
+
+const bird = ["sparrow", "dove", "crow", "peacock", "parrot", "pigeon", "owl"];
+bird [0] = ["hen"];
+bird.push["bat"];
+console.log(bird);
+
+
+const animal = ["rat", "cat", "goat", "sheep", "dog", "camel", "horse"];
+animal [0] = ["fox"];
+animal.push["lion"];
+animal.length[3]
+ console.log(animal);
+ 
+
+const vegetables = ["brinjal", "bean", "okra", "radish", "mint", "cucumber", "carrot"];
+vegetables [0] =["patato"];
+vegetables.push["cabbage"];
+console.log(vegetables);
+
+
+const color = ["red", "pink", "black","white","orange","pruple","grays","zink"];
+color [0] =["green"];
+color.push["blue"];
+console.log(color);
+console.log(color[3]);
+animal.length[8]
+
+
+const sports= ["football", "tennis", "hockey", "cricket", "volleyball"];
+sports [0] = ["hockey"];
+sports.push["tennis"];
+console.log(sports);
+
+
+const person = ["asma", "fatima", "amina", "maryam", "mahyam", "nimra","ayesha"];
+person [0] = ["sana"];
+console.log(person);
+
+
+const name= ["sana", "nimra", "eman", "rabia","naseem","mahreen","samreen"];
+name [0] = ["khadja"];
+console.log(name);
+
+
+const game= ["football", "tennis", "hockey", "cricket", "volleyball"];
+game [0] = ["hockey"];
+game.push["tennis"];
+console.log(game);
+
